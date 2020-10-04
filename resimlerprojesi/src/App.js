6226c251571ae98950d46801e186e983e6a05711
bsaglam <burcu.saglam001@gmail.com>
2020-10-04 04:13:07 +0300

    axios ile unsplash apiden rresimler çekildi

--- resimlerprojesi/src/App.js
+++ resimlerprojesi/src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar/searchBar'
 import './app.css'
+import axios from 'axios'
 
 export default class App extends Component {
-    onSearchImage = (deger)=>
+    state=
     {
-        console.log("App: "+deger);
+        images: []
+    }
+    onSearchImage = async (deger) => {
+        
+      const result= await axios.get("https://api.unsplash.com/search/photos",
+            {
+                params: { query: deger },
+                headers: { Authorization: "Client-ID cJbTvZFp7dInIeH7jD_YuXXjgxHiJ5fJRi88wGUjInM" }
+            })
+        this.setState({images: result.data.results});
     }
     render() {
         return (
-            <div className="app-container"> 
-               <SearchBar onSearchImage={this.onSearchImage}/> 
+            <div className="app-container">
+                <SearchBar onSearchImage={this.onSearchImage} />
+                <div>{this.state.images.length} resim bulundu</div>
             </div>
         )
     }
